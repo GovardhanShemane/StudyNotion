4106b0224f386dca3808a7d9165274a8d169d3aa
Govardhan  Shemane <140930185+GovardhanShemane@users.noreply.github.com>
2024-12-23 21:38:25 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
             >
                 <Box sx={{ color: "white", marginRight: "100px" }}>
                     <Typography sx={{ fontWeight: "bold", fontSize: "40px" }}>Hackathon Submissions</Typography>
-                    <Typography sx={{ m: "20px 0px", fontSize: "20px", overflow: "wrap", fontFamily: "inherit" }}>ur adipisicing elit. Magnam aliquid rerum sunt fuga. Dolor omnis obcaecati excepturi velit libero vitae quia minima, maxime molestiae animi architecto unde nobis rerum </Typography>
+                    <Typography sx={{ m: "20px 0px", fontSize: "20px", overflow: "wrap", fontFamily: "inherit" }}>Welcome innovators! Showcase your groundbreaking ideas and let your project shine—submit your hackathon masterpiece today and inspire creativity worldwide </Typography>
                     <Button component={Link} to="/addSubmission" sx={{ height: "50px", padding: "10px 20px", color: "white", backgroundColor: "#44924C", textTransform: "capitalize", fontWeight: "bold", fontSize: "15px", borderRadius: "12px", margin: "20px 0px", '&:hover': { backgroundColor: '#44934C', }, }}>Upload Submission</Button>
                 </Box>
                 <CardMedia
